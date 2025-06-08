3c790cc22937ff30d4c21c2d70a42b527d21edb1
sharit vaishnav <sharitvaishnav@Mac.lan>
2025-06-09 02:14:25 +0530

    tailwind config update

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
 module.exports = {
-  plugins: [
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
-    require('postcss-normalize'),
-    require('@tailwindcss/postcss7-compat'),
-    require('autoprefixer'),
-  ],
+    }
+  }
 } 
